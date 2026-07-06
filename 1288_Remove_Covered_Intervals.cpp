f0d506668e3421a5801b1c0bf00066879ebaee8c
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-06 14:00:15 +0530

    Sync: 1288. Remove Covered Intervals (Medium) - Leet2Git

--- 1288_Remove_Covered_Intervals.cpp
+++ 1288_Remove_Covered_Intervals.cpp
@@ -4,14 +4,15 @@ public:
         sort(intervals.begin(),intervals.end());
         int n=intervals.size(), ans=1;
         for(int i=1;i<n;i++){
-            cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
             if(intervals[i][0]>=intervals[i-1][0] && intervals[i][1]<=intervals[i-1][1]){
+                cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
                 continue;
             } else{
                 if(intervals[i][0]==intervals[i-1][0] && intervals[i][1]>=intervals[i-1][1]){
+                   cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
                     continue;
                 }
-                
+                cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;              
                 ans++;
             }
         }
