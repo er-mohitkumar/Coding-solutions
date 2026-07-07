e5908eec729a9a7357a1904a73493966189e6da5
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-07 13:23:43 +0530

    Sync: 3754. Concatenate Non-Zero Digits and Multiply by Sum I (Easy) - Leet2Git

--- 3754_Concatenate_Non_Zero_Digits_and_Multiply_by_Sum_I.cpp
+++ 3754_Concatenate_Non_Zero_Digits_and_Multiply_by_Sum_I.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        long long x = 0, sumX = 0, newX=0;
+        long long x = 0, sumX = 0, newX=0, place = 1;
         while(n){
             if(n%10 != 0){
-                x = (x*10) + (n%10);
+                x = ((n%10) * place) + (x*10);
                 sumX = sumX+(n%10);
             }
             n = n/10;
-        }
-        while(x){
-            newX = (newX*10) + (x%10);
-            x = x/10;
         }
         return newX*sumX;
     }
