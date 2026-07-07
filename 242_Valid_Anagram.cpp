e39608fcc5becb0c6840ae5ffa8fa14aed54bba4
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-07 17:44:03 +0530

    Sync: 242. Valid Anagram (Easy) - Leet2Git

--- 242_Valid_Anagram.cpp
+++ 242_Valid_Anagram.cpp
@@ -2,17 +2,25 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char,int>mp;
+        unordered_map<char,int>xp;
         for(char i:t){
             mp[i]++;
         }
         for(char i:s){
+            xp[i]++;
+        }
+        for(char i:s){
             if(mp.find(i)!=mp.end()){
                 mp[i]--;
+                xp[i]--;
                 if(mp[i]==0){
                     mp.erase(i);
                 }
+                if(xp[i]==0){
+                    xp.erase(i);
+                }
             }
         }
-        return mp.size()==0?true:false;
+        return mp.size()==0 && xp.size()==0?true:false;
     }
 };
