4a96317a11fde2debedb4abb4b454cd95e08738a
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-09 17:25:01 +0530

    Sync: 3532. Path Existence Queries in a Graph I (Medium) - Leet2Git

--- 3532_Path_Existence_Queries_in_a_Graph_I.cpp
+++ 3532_Path_Existence_Queries_in_a_Graph_I.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
         unordered_map<int,int>mp;
+        mp[nums[0]] = nums[0];
         for(int i=1;i<nums.size();i++){
             if(abs(nums[i]-nums[i-1])<=maxDiff){
                 mp[nums[i-1]] = nums[i];
@@ -9,10 +10,6 @@ public:
         }
 
         vector<bool> ans(queries.size(), true);
-
-        for(int i=0;i<ans.size();i++){
-            cout<<ans[i]<<" ";
-        }
 
         for(int i=0;i<queries.size();i++){
             int a = queries[i][0];
