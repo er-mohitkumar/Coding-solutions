eaaa1c3b52f2d9eb5b5064021851defcca9eac35
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-14 17:38:41 +0530

    Sync: 1291. Sequential Digits (Medium) - Leet2Git

--- 1291_Sequential_Digits.cpp
+++ 1291_Sequential_Digits.cpp
@@ -3,9 +3,11 @@ public:
     vector<int> sequentialDigits(int low, int high) {
         int n = low, place = 1, zeroth = 0;
         while(n){
-            place = place * 10;
             zeroth = n%10;
             n = n/10;
+            if(n){
+                place = place * 10;
+            }
         }
 
         cout<<zeroth<<endl;
