4e7cbdf1bb43498d21a6afb549a1fb4330f78cf8
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-14 18:23:24 +0530

    Sync: 1291. Sequential Digits (Medium) - Leet2Git

--- 1291_Sequential_Digits.cpp
+++ 1291_Sequential_Digits.cpp
@@ -1,52 +1,39 @@
 class Solution {
 public:
 
-    int n = 0, place = 1, zeroth = 0, first_no = 0, next = 0;
-
-    void find_next(int x, int plac, int zero){
-        while(x){
-            zero = x%10;
-            x = x/10;
-            if(x){
-                plac = plac * 10;
-            }
-        }
-        place = plac;
-        zeroth = zero;
-    }
-
-    void find_first(int plac, int first_n, int &nxt, int &zero){
-        while(plac!=1){
-            plac = plac/10;
-            ++zero;
-            first_n = (first_n * 10) + zero;
-            nxt = (nxt * 10) + 1;
-        }
-        place = plac;
-        first_no = first_n;
-        next = nxt;
-        zeroth = zero;
-    }
-
     vector<int> sequentialDigits(int low, int high) {
         vector<int>ans;
-        place = 1, zeroth = 0;
-        find_next(low,place,zeroth);
-        first_no = zeroth, next=1;
-        find_first(place, first_no, next, zeroth);
+        int n = low, place = 1, zeroth = 0;
+        while(n){
+            zeroth = n%10;
+            n = n/10;
+            if(n){
+                place = place * 10;
+            }
+        }
+        int first_no = zeroth, next=1, digits = place;
+        while(place!=1){
+            place = place/10;
+            ++zeroth;
+            first_no = (first_no * 10) + zeroth;
+            next = (next * 10) + 1;
+        }
         while(first_no<high){
             ans.push_back(first_no);
             first_no += next;
-            if(first_no%10 == 9){
+            if(first_no%10 ==9){
                 ans.push_back(first_no);
-                first_no+=next;
-                cout<<first_no<<endl;
-                place = 1;
-                zeroth = 0;
-                find_next(first_no, place, zeroth);
-                first_no = zeroth;
-                next=1;
-                find_first(place, first_no, next, zeroth);
+                first_no = 1;
+                place = digits * 10;
+                next = 1;
+                zeroth = 1;
+                while(place!=1){
+                    place = place/10;
+                    ++zeroth;
+                    first_no = (first_no * 10) + zeroth;
+                    next = (next * 10) + 1;
+                }
+                digits = place;
             }
         }
         return ans;
