1582f34717cce8a736f3fcf3ccc7b7be9da78be8
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-17 13:07:55 +0530

    Sync: 3658. GCD of Odd and Even Sums (Easy) - Leet2Git

---
+++ 3658_GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    int gcd(int a, int b)
+    {
+        if(a==0 || b==0) return max(a, b);
+        
+        int result = min(a, b);
+        while (result > 0) {
+            if (a % result == 0 && b % result == 0) {
+                break;
+            }
+            result--;
+        }
+        return result;
+    }
+
+    int gcdOfOddEvenSums(int n) {
+        int odd = 1, even = 2;
+        while(n--){
+            odd+=2;
+            even+=2;
+        }
+        return gcd(odd,even);
+    }
+};
