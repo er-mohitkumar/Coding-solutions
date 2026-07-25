d26d2b0cb26756a1e24e7662bbf3719d0181dcc6
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-25 19:16:33 +0530

    Sync: 1979. Find Greatest Common Divisor of Array (Easy) - Leet2Git

---
+++ 1979_Find_Greatest_Common_Divisor_of_Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int a = 0, b = 0, x = n;
+        while(x){
+            if(x==n){
+                a = x%10;
+            } else {
+                if(a<x%10){
+                    b = a;
+                    a = x%10;
+                } else if(b<x%10){
+                    b = x%10;
+                }
+            }
+            x = x/10;
+        }
+        return a*b;
+    }
+};
