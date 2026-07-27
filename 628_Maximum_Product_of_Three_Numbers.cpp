61dd903309520a90803f58d8c2f68e79e8f2c7e1
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-27 16:18:09 +0530

    Sync: 628. Maximum Product of Three Numbers (Easy) - Leet2Git

--- 628_Maximum_Product_of_Three_Numbers.cpp
+++ 628_Maximum_Product_of_Three_Numbers.cpp
@@ -1,47 +1,8 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        int a = nums[0], b = nums[1], c = nums[2];
-        if(abs(nums[0])>abs(nums[1]) && abs(nums[0])> abs(nums[2])){
-            a = nums[0];
-            if(abs(nums[1])>abs(nums[2])){
-                b = nums[1];
-                c = nums[2];
-            } else {
-                b = nums[2];
-                c = nums[1];
-            }
-        } else if(abs(nums[1])>abs(nums[0]) && abs(nums[1])>abs(nums[2])){
-            a = nums[1];
-            if(abs(nums[0])>abs(nums[2])){
-                b = nums[0];
-                c = nums[2];
-            } else {
-                b = nums[2];
-                c = nums[0];
-            }
-        } else {
-            a = nums[2];
-            if(abs(nums[0])>abs(nums[1])){
-                b = nums[0];
-                c = nums[1];
-            } else {
-                b = nums[1];
-                c = nums[0];
-            }
-        }
-        for(int i=3;i<nums.size();i++){
-            if(abs(nums[i])>abs(a)){
-                c = b;
-                b = a;
-                a = nums[i];
-            } else if(abs(nums[i])>abs(b)){
-                c=b;
-                b=nums[i];
-            } else if(abs(nums[i])>abs(c)){
-                c = nums[i];
-            }
-        }
-        return a*b*c;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        return max(nums[0]*nums[1]*nums[n-1], nums[n-1]*nums[n-2]*nums[n-3]);
     }
 };
