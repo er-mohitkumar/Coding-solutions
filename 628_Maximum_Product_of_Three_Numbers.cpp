b05f3e76e26b2da0deda3beff0b0c91408413890
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-27 15:50:07 +0530

    Sync: 628. Maximum Product of Three Numbers (Easy) - Leet2Git

--- 628_Maximum_Product_of_Three_Numbers.cpp
+++ 628_Maximum_Product_of_Three_Numbers.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        int first = 0, second = 0;
+    int maximumProduct(vector<int>& nums) {
+        int a = INT_MIN, b = INT_MIN, c = INT_MIN;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]>first){
-                second = first;
-                first = nums[i];
-            } else if(nums[i]>second && nums[i]<=first){
-                second = nums[i];
+            if(nums[i]>a){
+                c = b;
+                b = a;
+                a = nums[i];
+            } else if(nums[i]>b && nums[i]<=a){
+                c=b;
+                b=nums[i];
+            } else if(nums[i]>c && nums[i]<=a && nums[i]<=b){
+                c = nums[i];
             }
         }
-        return (first-1)*(second-1);
+        return a*b*c;
     }
 };
