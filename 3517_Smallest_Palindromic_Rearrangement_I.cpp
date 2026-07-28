5db98cfd858227f184b49c41aa78b012b7d41b2a
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-07-28 15:47:42 +0530

    Sync: 3517. Smallest Palindromic Rearrangement I (Medium) - Leet2Git

--- 3517_Smallest_Palindromic_Rearrangement_I.cpp
+++ 3517_Smallest_Palindromic_Rearrangement_I.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         if(s.size()==1) return s;
+
         sort(s.begin(), s.end());
         string ans = "";
-        int j=s.size()-1;
-        if(s.size()%2==0){
+
+        int n=s.size(), j=n-1;
+        if(n%2==0){
             if(s[j]!=s[j-1]){
                 return ans+=s[j];
             } else {
@@ -23,13 +25,15 @@ public:
         }
 
         if(j<0) j=0;
-        while(j<s.size()){
+        while(j<n-1){
             ans+=s[j];
-            
             j+=2;
         }
         cout<<ans<<endl;
         string Ans = ans;
+        if(n%2!=0){
+            Ans += s[n-1];
+        }
         reverse(ans.begin(),ans.end());
         ans = Ans + ans;
         return ans;
