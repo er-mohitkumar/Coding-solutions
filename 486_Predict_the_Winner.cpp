8b3949514e7dacf28a467e8f42bb247150f2bb9d
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-08-04 16:11:18 +0530

    Sync: 486. Predict the Winner (Medium) - Leet2Git

--- 486_Predict_the_Winner.cpp
+++ 486_Predict_the_Winner.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int aliceWins(vector<int>&piles, int i, int j, int a, int b, bool aliceTurn, vector<vector<int>>&dp){
-        if(i==j){
-            if(aliceTurn){
-                a+=piles[i];
-            } else {
-                b+=piles[i];
-            }
-            return dp[i][j]=a-b;
-        }
+    int aliceWins(vector<int>&piles, int i, int j, vector<vector<int>>&dp){
+        if(i == j)
+            return piles[i];
 
-        if(dp[i][j]!=0){
+        if(dp[i][j] != INT_MIN)
             return dp[i][j];
-        }
 
-        int l = aliceWins(piles, i+1, j, aliceTurn?a+=piles[i]:a, aliceTurn?b:b+=piles[i], aliceTurn=!aliceTurn, dp);
-        int r = aliceWins(piles, i, j-1, aliceTurn?a+=piles[j]:a, aliceTurn?b:b+=piles[j], aliceTurn=!aliceTurn, dp);
-        return l-r<0?dp[i][j]=r:dp[i][j]=l;
+        int takeLeft = piles[i] - aliceWins(piles, i+1, j, dp);
+        int takeRight = piles[j] - aliceWins(piles, i, j-1, dp);
+
+        return dp[i][j] = max(takeLeft, takeRight);
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        return aliceWins(nums, 0, n-1, 0, 0, true, dp)>=0?true:false;
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+        return aliceWins(nums, 0, n-1, dp)>=0?true:false;
     }
 };
