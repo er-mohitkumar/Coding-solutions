b74d525e7348a0c7ce525d40c982350028f48b4f
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-08-04 16:12:13 +0530

    Sync: 877. Stone Game (Medium) - Leet2Git

--- 877_Stone_Game.cpp
+++ 877_Stone_Game.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    bool aliceWins(vector<int>&piles, int i, int j, int a, int b, bool aliceTurn, vector<vector<int>>&dp){
-        if(dp[i][j]!=-1){
-            if(dp[i][j]==1){
-                return true;
-            } else {
-                return false;
-            }
-        }
-        if(i==j-1){
-            if(piles[i]>=piles[j]){
-                if(aliceTurn){
-                    a+=piles[i];
-                } else {
-                    b+=piles[j];
-                }
-            } else{
-                if(aliceTurn){
-                    a+=piles[j];
-                } else {
-                    b+=piles[i];
-                }
-            }
-            return a>b?dp[i][j]=1:dp[i][j]=0;
-        }
-        aliceTurn = !aliceTurn;
-        dp[i+1][j] = aliceWins(piles, i, j-1, a += piles[i], b, aliceTurn, dp);
-        dp[i][j-1] = aliceWins(piles, i+1, j, a, b += piles[j], aliceTurn, dp);
-        return dp[i][j];
+        int aliceWins(vector<int>&piles, int i, int j, vector<vector<int>>&dp){
+        if(i == j)
+            return piles[i];
+
+        if(dp[i][j] != INT_MIN)
+            return dp[i][j];
+
+        int takeLeft = piles[i] - aliceWins(piles, i+1, j, dp);
+        int takeRight = piles[j] - aliceWins(piles, i, j-1, dp);
+
+        return dp[i][j] = max(takeLeft, takeRight);
     }
+
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return aliceWins(piles, 0, n-1, 0, 0, true, dp);
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+        return aliceWins(piles, 0, n-1, dp)>0?true:false;
     }
 };
