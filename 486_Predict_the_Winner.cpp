293911f3ce17c1b3a6c4635f86e4079b831c361d
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-08-04 14:37:55 +0530

    Sync: 486. Predict the Winner (Medium) - Leet2Git

--- 486_Predict_the_Winner.cpp
+++ 486_Predict_the_Winner.cpp
@@ -16,12 +16,12 @@ public:
 
         int l = aliceWins(piles, i+1, j, aliceTurn?a+=piles[i]:a, aliceTurn?b:b+=piles[i], aliceTurn=!aliceTurn, dp);
         int r = aliceWins(piles, i, j-1, aliceTurn?a+=piles[j]:a, aliceTurn?b:b+=piles[j], aliceTurn=!aliceTurn, dp);
-        return dp[i][j]=l-r;
+        return l-r<0?dp[i][j]=r:dp[i][j]=l;
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int>(n, 0));
-        return aliceWins(nums, 0, n-1, 0, 0, true, dp)<=0?true:false;
+        return aliceWins(nums, 0, n-1, 0, 0, true, dp)>=0?true:false;
     }
 };
