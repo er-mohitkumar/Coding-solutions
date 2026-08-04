7d082cf7eacda4cc2a96756956ed0d0861becbbe
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-08-04 18:25:40 +0530

    Sync: 3693. Climbing Stairs II (Medium) - Leet2Git

--- 3693_Climbing_Stairs_II.cpp
+++ 3693_Climbing_Stairs_II.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int minCost(int n, int i, int j, vector<int>& costs, vector<int>& dp){
-        if(j>=n-1){
+    int minCost(int n, int i, vector<int>& costs, vector<int>& dp){
+        if(i>=n-1){
             return 0;
         }
         if(dp[i]!=INT_MAX){
             return dp[i];
         }
-        int ans = INT_MAX;
-        ans = min(ans, costs[j]+((j-i)*(j-i)) + minCost(n,i+1,j+1,costs,dp));
-        if(j+1<=n){
-            i+=1;
-            j+=1;
-            ans = min(ans, costs[j]+((j-i)*(j-i)) + minCost(n,i+1,j+1,costs,dp));
+        int ans = INT_MAX, j=i+1;
+        ans = min(ans, costs[j]+((j-i)*(j-i)) + minCost(n,i+1,costs,dp));
+        j=i+1;
+        if(i+1<n){
+            ans = min(ans, costs[j]+((j-i)*(j-i)) + minCost(n,i+2,costs,dp));
         }
-        if(j+2<=n){
-            i+=1;
-            j+=1;
-            ans = min(ans, costs[j]+((j-i)*(j-i)) + minCost(n,i+1,j+1,costs,dp));
+        j=i+2;
+        if(i+2<n){
+            ans = min(ans, costs[j]+((j-i)*(j-i)) + minCost(n,i+3,costs,dp));
         }
         return dp[i]=ans;
     }
     int climbStairs(int n, vector<int>& costs) {
         vector<int>dp(n+1,INT_MAX);
-        return minCost(n,0,1, costs, dp);
+        return minCost(n,0, costs, dp);
     }
 };
