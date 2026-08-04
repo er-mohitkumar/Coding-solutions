1882c9088bf8f275d257e9ac1946cf281f5bf644
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-08-04 17:14:44 +0530

    Sync: 1406. Stone Game III (Hard) - Leet2Git

--- 1406_Stone_Game_III.cpp
+++ 1406_Stone_Game_III.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-        int aliceWins(vector<int>&piles, int i, int j, vector<vector<int>>&dp){
-        if(i == j)
+
+    int aliceWins(vector<int>&piles, int i, int j, vector<vector<int>>&dp){
+        if (i > j)
+            return 0;
+
+        if (i == j)
             return piles[i];
 
         if(dp[i][j] != INT_MIN)
             return dp[i][j];
 
-        int takeLeft = piles[i] - aliceWins(piles, i+1, j, dp);
-        int takeRight = piles[j] - aliceWins(piles, i, j-1, dp);
-
-        return dp[i][j] = max(takeLeft, takeRight);
+        int first = piles[i] - aliceWins(piles, i+1, j, dp);
+        if(i+2<=j){
+            int second = piles[i+1] - aliceWins(piles, i+2, j, dp);
+            if(i+3<=j){
+                int third = piles[i+2] - aliceWins(piles, i+3, j, dp);
+                return dp[i][j] = max(max(first, second), third);
+            }
+            return dp[i][j] = max(first, second);
+        }
+        return dp[i][j] = first;
     }
 
-    bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        if(n<=3) return "Alice";
         vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
-        return aliceWins(piles, 0, n-1, dp)>0?true:false;
+        int ans = aliceWins(stoneValue, 0, n-1, dp);
+        if(ans>0){
+            return "Alice";
+        } else if(ans<0){
+            return "Bob";
+        } 
+        return "Tie";
     }
 };
