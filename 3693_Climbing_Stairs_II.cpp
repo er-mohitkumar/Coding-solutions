60b83cfd40e15abc90541a179be468fc871b07af
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-08-04 18:28:20 +0530

    Sync: 3693. Climbing Stairs II (Medium) - Leet2Git

--- 3693_Climbing_Stairs_II.cpp
+++ 3693_Climbing_Stairs_II.cpp
@@ -9,18 +9,22 @@ public:
         }
         int ans = INT_MAX, j=i+1;
         ans = min(ans, costs[j]+((j-i)*(j-i)) + minCost(n,i+1,costs,dp));
+        cout<<ans<<" ";
         j=i+1;
         if(i+1<n){
             ans = min(ans, costs[j]+((j-i)*(j-i)) + minCost(n,i+2,costs,dp));
+            cout<<ans<<" ";
         }
         j=i+2;
         if(i+2<n){
             ans = min(ans, costs[j]+((j-i)*(j-i)) + minCost(n,i+3,costs,dp));
+            cout<<ans<<" ";
         }
+        cout<<endl;
         return dp[i]=ans;
     }
     int climbStairs(int n, vector<int>& costs) {
         vector<int>dp(n,INT_MAX);
-        return minCost(n,0, costs, dp);
+        return minCost(n, 0, costs, dp);
     }
 };
