9d8f1305ba17b5fcd6f72963d228e779166902ff
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-08-04 14:12:27 +0530

    Sync: 486. Predict the Winner (Medium) - Leet2Git

--- 486_Predict_the_Winner.cpp
+++ 486_Predict_the_Winner.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    bool aliceWins(vector<int>&piles, int i, int j, int a, int b, bool aliceTurn, vector<vector<int>>&dp){
-        if(dp[i][j]!=-1){
-            if(dp[i][j]==1){
-                return true;
+    int aliceWins(vector<int>&piles, int i, int j, int a, int b, bool aliceTurn, vector<vector<int>>&dp){
+        if(i==j){
+            if(aliceTurn){
+                a+=piles[j];
             } else {
-                return false;
-            }
-        }
-        if(i==j-1){
-            if(piles[i]>=piles[j]){
-                if(aliceTurn){
-                    a+=piles[i];
-                } else {
-                    b+=piles[j];
-                }
-            } else{
-                if(aliceTurn){
-                    a+=piles[j];
-                } else {
-                    b+=piles[i];
-                }
+                b+=piles[i];
             }
             return dp[i][j]=a-b;
+        }
+
+        if(dp[i][j]!=0){
+            return dp[i][j];
         }
 
         if(aliceTurn){
@@ -31,14 +20,14 @@ public:
             b+=piles[i];
         }
         aliceTurn = !aliceTurn;
-        int r = aliceWins(piles, i+1, j, a, b, aliceTurn, dp);
-        int l = aliceWins(piles, i, j-1, a, b, aliceTurn, dp);
-        return dp[i][j]=r-l;
+        int l = aliceWins(piles, i+1, j, a, b, aliceTurn, dp);
+        int r = aliceWins(piles, i, j-1, a, b, aliceTurn, dp);
+        return dp[i][j]=l-r;
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return aliceWins(nums, 0, n-1, 0, 0, true, dp);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        return aliceWins(nums, 0, n-1, 0, 0, true, dp)>0?true:false;
     }
 };
