63ee9893f7cfef5f5f49421a702a9d008274d46b
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2026-08-22 20:14:23 +0530

    Sync: 3. Longest Substring Without Repeating Characters (Medium) - Leet2Git

--- 3_Longest_Substring_Without_Repeating_Characters.cpp
+++ 3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string arr) {
         int n = arr.size(), right = 0, left = 0, maxStringLength = INT_MIN, stringLength = 0;
-    unordered_map<char,int> characters;
-    while(right<n){
-        if(characters.find(arr[right])!=characters.end()){
-            stringLength = characters.size();
-            maxStringLength = max(maxStringLength, stringLength);
-            while(arr[left]!=arr[right]){
-                characters[arr[left]]--;
-                if(characters[arr[left]]==0) characters.erase(arr[left]);
-                left++;
-            }
-        } 
-        characters[arr[right]]++;
-        right++;
-    }
-    stringLength = characters.size();
-    maxStringLength = max(maxStringLength, stringLength);
+        unordered_map<char,int> characters;
+        while(right<n){
+            if(characters.find(arr[right])!=characters.end()){
+                stringLength = characters.size();
+                maxStringLength = max(maxStringLength, stringLength);
+                do {
+                    characters[arr[left]]--;
+                    if(characters[arr[left]]==0) characters.erase(arr[left]);
+                    left++;
+                } while(arr[left]!=arr[right]);
+            } 
+            characters[arr[right]]++;
+            right++;
+        }
+        stringLength = characters.size();
+        maxStringLength = max(maxStringLength, stringLength);
 
     return maxStringLength;
     }
